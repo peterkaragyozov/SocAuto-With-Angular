05f7183bc47575e3e1620eb9ff716fae936ee692
Peter Karagyozov <pepitoitalianeca@yahoo.com>
2021-08-04 23:54:55 +0300

    Minor fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { HomeComponent } from './home/home.component';
+import { CatalogComponent } from './catalog/catalog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CatalogComponent
   ],
   imports: [
     BrowserModule,
