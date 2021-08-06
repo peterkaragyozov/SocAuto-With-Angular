3dd4cab6f41d4988f2c30aba3b012c20cea90c61
Peter Karagyozov <pepitoitalianeca@yahoo.com>
2021-08-07 00:21:24 +0300

    Fixed bug

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -4,39 +4,47 @@ import { AuthActivate } from '../core/guards/auth.activate';
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
 import { RegisterComponent } from './register/register.component';
+import { NotFoundComponent } from '../not-found/not-found.component'
 
 const routes: Routes = [
-    {
-        path: 'login',
-        component: LoginComponent,
-        canActivate: [AuthActivate],
-        data: {
-            authenticationRequired: false,
-            authenticationFailureRedirectUrl: '/',
-          }
+  {
+    path: 'login',
+    component: LoginComponent,
+    canActivate: [AuthActivate],
+    data: {
+      authenticationRequired: false,
+      authenticationFailureRedirectUrl: '/',
     },
-    {
-        path: 'register',
-        component: RegisterComponent,
-        canActivate: [AuthActivate],
-        data: {
-            authenticationRequired: false,
-            authenticationFailureRedirectUrl: '/',
-          }
+  },
+
+  {
+    path: 'register',
+    component: RegisterComponent,
+    canActivate: [AuthActivate],
+    data: {
+      authenticationRequired: false,
+      authenticationFailureRedirectUrl: '/',
     },
-    {
-        path: 'profile',
-        component: ProfileComponent,
-        canActivate: [AuthActivate],
-        data: {
-            authenticationRequired: true,
-            authenticationFailureRedirectUrl: '/login',
-          }
-    }
+  },
+
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    canActivate: [AuthActivate],
+    data: {
+      authenticationRequired: true,
+      authenticationFailureRedirectUrl: '/login',
+    },
+  },
+
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class UserRoutingModule { }
+export class UserRoutingModule {}
