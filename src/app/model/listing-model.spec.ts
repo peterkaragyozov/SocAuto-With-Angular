0130f7959438209d8150464103616b0b9344ed5d
Peter Karagyozov <pepitoitalianeca@yahoo.com>
2021-08-10 00:33:31 +0300

    Removed unnecessary files

--- src/app/model/listing-model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ListingModel } from './listing-model';
-
-describe('ListingModel', () => {
-  it('should create an instance', () => {
-    expect(new ListingModel()).toBeTruthy();
-  });
-});
